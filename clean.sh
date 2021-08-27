f5d5733dddf8a0c7a8004ba0dcd576b109c9d0fa
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-08-27 14:00:43 +0000

    Boot lineage

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
+reset
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 export CROSS_COMPILE=./toolchain/gcc/linux-x86/aarch64/aarch64-gcc-4.9-brillo-m10-release/bin/aarch64-linux-android-
 
-make ARCH=arm64 exynos7885-gta3xlwifi_defconfig
-make ARCH=arm64 -j8
+make mrproper
+make clean
